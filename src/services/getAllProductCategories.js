7907a7add46112a3e34d79cdb9b4282e7029250b
Veyll <134904004+AlhadiDewana@users.noreply.github.com>
2024-11-18 13:45:32 +0700

    Add files via upload

---
+++ src/services/getAllProductCategories.js
@@ -0,0 +1,13 @@
+export default function getAllProductCategories(){
+  return  [   {
+      id: 'Gundam',
+      slug: 'gundam',
+      name: `Gundam`
+    },
+    {
+      id: 'PGundam',
+      slug: 'pokemon-gundam',
+      name: `Pokemon Gundam`
+    },
+  ]
+}
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,65 @@
+export const getAllProducts = () => {
+  return [
+    //1
+    {
+      id: 'GMFG-Gundam-001',
+      slug: 'freedom-gundam-men',
+      name: "Gundam Mg 1/100 Freedom Gundam - GDM061611",
+      category: `Gundam`,
+      categorySlug: 'gundam',
+      price: 899_000,
+      stock: 100,
+      imageUrl: '/assets/images/Freedom-Gundam.jpg',
+      description: `Kira Yamato’s mobile suit Freedom Gundam from the hit series “Gundam Mobile Suit Gundam SEED” and “Mobile Suit Gundam SEED Destiny” gets an updated Master Grade 2.0 release from Bandai! This new kit has been redesigned with fantastic details and improved articulation! All parts come molded in multiple colors and features a snap fit construction.`
+    },
+    //2
+    {
+      id: 'GHGHA-PGundam-004',
+      slug: 'Gundam-Astray-pgundam',
+      name: "Gundam 1/144 Hg Gundam Astray (Blue Frame) - GDM060358",
+      category: `Gundam`,
+      categorySlug: 'gundam',
+      price: 279_000,
+      stock: 50,
+      imageUrl: `/assets/images/Astray-Gundam.jpg`,
+      description: `This crisp new Gundam Astray (blue frame) comes to us in a High Grade model, superbly molded in color and of course featuring snap-fit assembly for ease in building. Nicely engineered, it's extremely poseable thanks to matching blue polycaps (instead of the standard grey!), and can even drop to one knee to use the included shoulder-mount bazooka! Foil stickers for shiny bits and clear stickers for other markings are included as well, for added detail.`
+    },
+    //3
+    {
+      id: 'GPSSBK-PGundam-003',
+      slug: 'pokepla-gundam-kyurem-pgundam',
+      name: "Gundam POKEPLA SELECT SERIES BLACK KYUREM - GDM5058291",
+      category: `Pokemon Gundam`,
+      categorySlug: 'pokemon-gundam',
+      price: 153_000,
+      stock: 0,
+      imageUrl: `/assets/images/Pokepla-Gundam.jpg`,
+      description: `Bandai Pokemon Plamo 27 Black Kyurem (Plastic Model Kit) Item is a plastic model kit and assembly is required Our items are Bandai original items distributed in Japan and comes with Japanese instructions (illustrated diagram is included) Made in Japan.`
+    },
+    //4
+    {
+      id: 'PPCSSG-PGundam-002',
+      slug: 'gengar-gundam-pgundam',
+      name: "Pokémon PLAMO COLLECTION 45 SELECT SERIES Gengar - 060441",
+      category: `Pokemon Gundam`,
+      categorySlug: 'pokemon-gundam',
+      price: 239_000,
+      stock: 5,
+      imageUrl: `/assets/images/Gengar-Gundam.jpg`,
+      description: `The popular ghost type Pokemon Gengar is now available in Bandai's Pokemon Plamo Select Series! In addition to touch gate and snap fit technology, the number of stickers have been greatly reduced, and assembly is easy! Gengar comes with movable joints for posing and different facial and mouth parts. Catch this Pokemon for your collection today! [Set Contents]: Face parts x 2 Tongue parts x 2 Foil seal x 1.`
+    },
+    //5
+    {
+      id: 'PGPCQC-PGundam-005',
+      slug: 'pokepla-gundam-Charmender-pgundam',
+      name: "Pokémon PLAMO COLLECTION QUICK!! 11 Charmander - 063366",
+      category: `Pokemon Gundam`,
+      categorySlug: 'pokemon-gundam',
+      price: 127_000,
+      stock: 0,
+      imageUrl: `/assets/images/Charmender-Gundam.jpg`,
+      description: `Charmander is now part of Bandai's "Pokemon Plamo Collection Quick!!" model-kit lineup! He's easy and fun to assemble, with pre-colored parts that can be removed from the sprues by hand. Stickers are included for his markings. You'll love it -- order yours now! [Size]: Approximately 8.7cm tall when completed [Includes]: Stickers.`
+    },
+
+  ]
+}
